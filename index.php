73f7c644b11a6908c482cf9c8ed8ae76a90d340b
K44aue <128631337+K44aue@users.noreply.github.com>
2023-03-22 16:22:18 -0300

    modificaçao

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <body>
         <h1>Título da Página</h1>
         <?php
-        //lucas, amanda terzinha, gabriely, divid, 
+        //KAUE, 
             echo "uma frase criativa"."<br>";
             $servidor = "localhost";
             $usuario = "eumesmo";
@@ -20,6 +20,49 @@
                 die("Conexão Falhou:".$conn -> connect_error);
             }
             echo "Conectado ao DB"."<br>";
+
+            $sql="SELECT Nome ,Sobrenome,Telefone from agenda";
+            $result=$conn->query ($sql);
+            echo
+
+           " <table>
+            <thead>
+                <th>Nome</th>
+                <th>sobrenome</th>
+                <th>telefone</th>
+            </thead>
+            <tbody>";
+                
+            
+
+            if($result -> num_rom>0){
+                while($rom=$result ->fetch_assor()){
+                    echo 
+                   "<tr>"
+                        . " <td>".rom["Nome"]."</td>"
+                        . " <td>".rom["Sobrenome"]."</td>"
+                        . " <td>".rom["Telefone"]."</td>"
+                    ."</tr>";
+                    echo
+                    "</tbody>
+                     </table>";
+                }
+            } else{
+                echo
+                "<tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr> ";
+                echo
+                "</tbody>
+                 </table> </br>";
+                 echo "<h4>0 resultados encontrados. A tabela está vazia.</h4>";
+
+            }
+
+           
+
         ?>
     </body>
 </html>
